88d242c3f08ec47cd31a0ee7e07999139d39591f
kaisyQ <rutyo8@mail.ru>
2023-09-27 15:51:32 +1000

    step2

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +11,41 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ConferenceController extends AbstractController
 {
+
+    public function __construct
+    (
+        private ConferenceRepository $conferenceRepository,
+        private CommentRepository $commentRepository
+    ) {}
+
     #[Route(path: '/', name: 'homepage')]
-    public function index(Request $request): Response
+    public function index(): Response
     {
-        $name = $request->query->get('name');
-        return $this->json([
-            'name' => $name
+        return $this->render('conference/index.html.twig', [
+            'conferences' => $this->conferenceRepository->findAll()
+        ]);
+    }
+
+    #[Route(path: '/conference/{id}', name: 'conference_show')]
+    public function show(int $id, Request $request) : Response
+    {
+
+
+        $conference = $this->conferenceRepository->find($id);
+
+        $pageNumber = max(1, $request->query->getInt('pageNumber', 1));
+
+        $paginator = $this->commentRepository->getCommentPaginator(
+            $conference, $pageNumber
+        );
+
+
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'nextPage' => $pageNumber + 1,
+            'prevPage' => $pageNumber < 2 ? null : $pageNumber - 1,
+            'currentPage' => $pageNumber
         ]);
     }
 }
---
+++ src/Repository/CommentRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Comment>
+ *
+ * @method Comment|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Comment|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Comment[]    findAll()
+ * @method Comment[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CommentRepository extends ServiceEntityRepository
+{
+
+    public const ITEMS_ON_PAGE = 2;
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Comment::class);
+    }
+
+
+    public function getCommentPaginator(Conference $conference, int $pageNumber=1) : Paginator 
+    {
+        $query = $this->createQueryBuilder('c')
+            ->where('c.conference = :conference')
+            ->setParameter('conference', $conference)
+            ->orderBy('c.createdAt', 'DESC')
+            ->setMaxResults(self::ITEMS_ON_PAGE)
+            ->setFirstResult(($pageNumber-1)*self::ITEMS_ON_PAGE)
+            ->getQuery();
+
+        return new Paginator($query);
+
+    }
+}
